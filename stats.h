6d027fe3f2aee0e3aed448ae46b5da5bca213e31
Katre <mithlesh.katre@philips.com>
2025-05-09 10:45:38 +0530

    added incomplated code

--- stats.h
+++ stats.h
@@ -1,9 +1,14 @@
 #include <vector>
 
+struct Stats {
+    float average;
+    float min;
+    float max;
+};
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
     
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<float>& );
 }
 
 
